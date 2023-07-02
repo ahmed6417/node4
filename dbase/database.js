6e700af288ad3c34d7bfa6a277e8478a0a462a75
AhmedHossam <ahmedhossam6417@gmail.com>
2023-07-02 13:52:53 +0300

    new

---
+++ dbase/database.js
@@ -0,0 +1,41 @@
+const productList = [
+    {
+        "id": 1,
+        "title": "kachu",
+        "price": 1,
+        "description": "A descriptionhhuhu",
+        "images": [
+        "https://placeimg.com/640/480/any"
+        ],
+        "categoryId": 1,
+        },
+        {
+        "id": 2,
+        "title": "New Product",
+        "price": 10,
+        "description": "A description",
+        "images": [
+        "https://placeimg.com/640/480/any"
+        ],
+        
+        "categoryId":1,
+
+        },
+        {
+        "id": 3,
+        "title": "New Product",
+        "price": 10,
+        "description": "A description",
+        "images": [
+        "https://placeimg.com/640/480/any"
+        ],
+
+        "categoryId": 1
+        },
+        
+]
+const getProduct = ()=>{
+    return productList;
+}
+
+module.exports = getProduct();
